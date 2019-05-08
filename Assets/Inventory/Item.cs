bff1467b384fc554b5e46ef31489e71aad9f62f9
Vanessa <vanessa.speeth@tu-dortmund.de>
2019-05-08 22:14:32 +0200

    Item Objects & Inventory first steps

---
+++ Assets/Inventory/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class Item : ScriptableObject
+{
+    public new string name;
+    public string description;
+    
+    public Sprite itemPicuture;
+    
+     
+}
---
+++ Assets/ShowItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowItem : MonoBehaviour
+{
+
+    public Item displayItem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Image currentImage = GetComponentInParent<Image>();
+        currentImage.sprite = displayItem.itemPicuture;
+        
+        
+        
+    }
+
+    private void OnMouseOver()
+    {
+        
+    }
+}
