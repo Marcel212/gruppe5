f810582fdbc733aeb68e44b95b15032f23c4a77e
Vanessa <vanessa.speeth@tu-dortmund.de>
2019-05-08 23:11:42 +0200

    Bei Tastendruck öffnen. Hover noch nicht testbar, da Kamera noch nicht losgelöst.

---
+++ Assets/Inventory/InventoryControll.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryControll : MonoBehaviour
+{
+    private Boolean isActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            isActive = !isActive;
+            transform.GetChild(0).gameObject.SetActive(isActive);
+            
+        }
+
+
+    }
+}
+
---
+++ Assets/Inventory/ItemTextControll.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTextControll : MonoBehaviour
+{
+    public Transform popupText;
+
+    public static Boolean visible = false;
+
+
+    private void OnMouseEnter()
+    {
+        if (!visible)
+        {
+            popupText.GetComponent<TextMesh>().text = "Something new";
+            visible = true;
+            Instantiate(popupText, new Vector3(transform.position.x, transform.position.y + 2, 0), popupText.rotation);
+            
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (visible)
+        {
+            visible = false;
+            
+        }
+    }
+}
+ 
