bb2fde568fe8006d177ea8f06e4e19a3f90c80e9
Marcel <marcel212@live.de>
2019-05-14 13:48:44 +0200

    Maus bei Knopfdruck befreit und Kamera gestoppt. - Vanessa

--- Assets/Inventory/InventoryControll.cs
+++ Assets/Inventory/InventoryControll.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class InventoryControll : MonoBehaviour
 {
+    [SerializeField] private GameObject m_FPSController;
     private Boolean isActive;
     // Start is called before the first frame update
     void Start()
@@ -20,6 +22,7 @@ public class InventoryControll : MonoBehaviour
         {
             isActive = !isActive;
             transform.GetChild(0).gameObject.SetActive(isActive);
+            m_FPSController.GetComponent<FirstPersonController>().m_InventoryOpen = isActive;
             
         }
 
--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -27,6 +27,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
         [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
         [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
+        public bool m_InventoryOpen;
 
         private Camera m_Camera;
         private bool m_Jump;
@@ -58,10 +59,20 @@ namespace UnityStandardAssets.Characters.FirstPerson
         }
 
 
+        
+
         // Update is called once per frame
         private void Update()
         {
-            RotateView();
+            if (m_InventoryOpen)
+            {
+                m_MouseLook.SetCursorLock(false);
+            }
+            else
+            {
+                RotateView();
+            }
+            
             // the jump state needs to read here to make sure it is not missed
             if (!m_Jump)
             {
