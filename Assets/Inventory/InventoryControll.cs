e1169343f77552f21e2600820fc1a3b0540c4267
Vanessa <vanessa.speeth@tu-dortmund.de>
2019-05-18 15:23:59 +0200

    ItemDropped started, Slot as Prefeb. Inventory adust.

--- Assets/Inventory/InventoryControll.cs
+++ Assets/Inventory/InventoryControll.cs
@@ -6,7 +6,33 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 public class InventoryControll : MonoBehaviour
 {
-    public List<Item> itemsInInventory; 
+
+    public const int SLOTS = 35;
     
+    public List<IInventoryItem> itemsInInventory = new List<IInventoryItem>();
+
+    public event EventHandler<InventoryEventArgs> ItemAdded;
+
+    public void AddItem(IInventoryItem item)
+    {
+        if (itemsInInventory.Count < SLOTS)
+        {
+            Collider collider = (item as MonoBehaviour).GetComponent<Collider>();
+            if (collider.enabled)
+            {
+                collider.enabled = false;
+                
+                itemsInInventory.Add(item);
+                
+                item.OnPickUp();
+                
+            }
+        }
+
+        if (ItemAdded != null)
+        {
+            ItemAdded(this, new InventoryEventArgs(item));
+        }
+    }
 }
 
