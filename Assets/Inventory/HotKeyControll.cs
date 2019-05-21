1b38b3fcce89d4d771193e080f5ed3529f77ec1b
Marcel <marcel212@live.de>
2019-05-21 16:07:03 +0200

    HotKeyInventar mit HotKey verknüpft.

--- Assets/Inventory/HotKeyControll.cs
+++ Assets/Inventory/HotKeyControll.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HotKeyControll : MonoBehaviour
 {
@@ -8,14 +9,27 @@ public class HotKeyControll : MonoBehaviour
     // TODO Mit HotKeyPanel im Inventar verknüpfen
     // TODO Auf Tasten 1-0 Reagieren lassen 
     // Start is called before the first frame update
+
+    [SerializeField] private GameObject hotKeyInventory;
     void Start()
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            // Verknüpfe jeden Hotkey unten mit dem Hotkey aus dem Inventar 
+            Item itemInInventory = hotKeyInventory.transform.GetChild(i).GetChild(0).GetChild(0)
+                .GetComponent<ShowItem>().itemToShow;
+            Transform childWithItem = transform.GetChild(i).GetChild(0).GetChild(0);
+            childWithItem.GetComponent<ShowItem>().itemToShow = itemInInventory;
+            childWithItem.GetComponent<Image>().enabled = true;
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /*
+     * Wird aufgerufen, wenn im Inventar etwas zum HotKey hinzugefügt wird.
+     */
+    public void AddToHotKey(int index, Item itemToAdd)
     {
-        
+        transform.GetChild(index).GetChild(0).GetChild(0).GetComponent<ShowItem>().itemToShow = itemToAdd;
     }
 }
