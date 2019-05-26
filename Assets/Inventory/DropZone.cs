ead47d10b1b93b213d484288b26a95875e968fc6
Vanessa <vanessa.speeth@tu-dortmund.de>
2019-05-26 17:05:49 +0200

    DropZone verändert, Namensänderungen

--- Assets/Inventory/DropZone.cs
+++ Assets/Inventory/DropZone.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
-public class ItemDropHandler : MonoBehaviour, IDropHandler
+public class DropZone : MonoBehaviour, IDropHandler
 {
- 
+    public Placement m_placement;
+     
+    
+    
+    
+    
     public void OnDrop(PointerEventData eventData)
     {
         RectTransform invPanel = transform as RectTransform;
@@ -14,35 +18,42 @@ public class ItemDropHandler : MonoBehaviour, IDropHandler
         ShowItem itemScriptDrag = eventData.pointerDrag.transform.GetComponent<ShowItem>();
         Item itemDragged = itemScriptDrag.itemToShow;
         
-        if (!RectTransformUtility.RectangleContainsScreenPoint(invPanel, Input.mousePosition))
+        Debug.Log("Befindet sich im Placement : " + m_placement) ;
+        if (itemDragged != null)
         {
-           //itemScriptDrag.itemToShow = null;
-            Debug.Log("Item wegwerfen");
-        }
-        else
-        {
-            //Debug.Log("Herkunft-Name: " + eventData.pointerDrag.transform.GetComponent<ShowItem>().itemToShow.name + " | Ziel-Name: " + eventData.pointerEnter.name);
-            
-            if (itemDragged != null)
+            if (m_placement == Placement.World)
+            {
+                itemScriptDrag.itemToShow = null;
+                // TODO Gameobject erzeugen
+                Debug.Log("Wegwerfen");
+            }
+            else if(m_placement == Placement.Inventory || m_placement == Placement.Hotkeys)
             {
                 //Script und Item vom Dropped item
                 ShowItem itemScriptDropped = eventData.pointerEnter.GetComponent<ShowItem>();
-               
+    
                 //Wenn ein Script existiert. 
                 if (itemScriptDropped != null)
                 {
                     Item itemOnDropped = itemScriptDropped.itemToShow;
-                    
+    
                     //Items tauschen
                     itemScriptDropped.itemToShow = itemDragged;
                     itemScriptDrag.itemToShow = itemOnDropped;
                 }
-                
-                
+
+                if (m_placement == Placement.Hotkeys)
+                {
+                    Debug.Log("HotKey Aktualisieren");
+                }
+
+            }else if (m_placement == Placement.Forbidden)
+            {
+                Debug.Log("Verboten");
             }
-            
-            
-            
         }
     }
+    
+    public enum Placement {Hotkeys, Forbidden, Inventory, World};
+
 }
